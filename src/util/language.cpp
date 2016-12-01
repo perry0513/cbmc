a3f6d88cc174d825579ac8bf0e0d92dae02ca875
thk123 <thomas@diffblue.com>
2016-12-01 16:06:24 +0000

    Generate stub method for opaque functions
    Initial commit making a stub function in the GOTO program for any opaque
    functions (i.e. functions for which we don't have a body). This is done
    to ensure we can match up the assignments after the opaque function to
    the return from the opaque function.

--- src/util/language.cpp
+++ src/util/language.cpp
@@ -11,6 +11,8 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #include "language.h"
 #include "expr.h"
+#include <util/symbol.h>
+#include <util/symbol_table.h>
 
 bool languaget::final(symbol_tablet &symbol_table)
 {
