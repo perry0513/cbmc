1f016ced7917e117becc2b09b1baa02ffd814d71
Elizabeth Polgreen <epolgreen@gmail.com>
2021-04-29 16:20:25 +0100

    stop outputting smt files

--- src/solvers/smt2/smt2_dec.cpp
+++ src/solvers/smt2/smt2_dec.cpp
@@ -19,8 +19,6 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #include "smt2irep.h"
 
-#include <iostream>
-
 std::string smt2_dect::decision_procedure_text() const
 {
   // clang-format off
@@ -50,7 +48,6 @@ decision_proceduret::resultt smt2_dect::dec_solve()
     std::ofstream problem_out(
       temp_file_problem(), std::ios_base::out | std::ios_base::trunc);
     problem_out << stringstream.str();
-    std::cout << stringstream.str()<<std::endl;
     write_footer(problem_out);
   }
 
